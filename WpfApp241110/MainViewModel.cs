11a64606140bcc3aac131adfcc5a4fc7734718f3
dubukimch <101459905+dubukimch@users.noreply.github.com>
2024-11-10 22:50:39 +0900

    예제 2: 부모 컨트롤에 DataContext 설정 #2
    부모 컨트롤(StackPanel)에 DataContext를 설정하여 해당 컨트롤의 모든 자식이 동일한 데이터 컨텍스트를 공유하도록 설정하는 예제입니다.

---
+++ WpfApp241110/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+
+namespace WpfApp241110
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _text;
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                if (_text != value)
+                {
+                    _text = value;
+                    OnPropertyChanged(nameof(Text));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged (string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
