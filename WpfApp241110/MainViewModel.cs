0a18b476eab294a5b0a240e31acd2ea5a681a28c
dubukimch <101459905+dubukimch@users.noreply.github.com>
2024-11-11 20:30:31 +0900

    INotifiPropertyChanged #7
    INotifyPropertyChanged를 구현하려면 클래스에서 PropertyChanged 이벤트와 OnPropertyChanged 메서드

---
+++ WpfApp241110/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+
+namespace WpfApp241111
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _userName;
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                if (_userName != value)
+                {
+                    _userName = value;
+                    OnPropertyChanged(nameof(UserName));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged (string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
