4d19d65f8df873fe846357106695bcfb82d6e904
dubukimch <101459905+dubukimch@users.noreply.github.com>
2024-11-18 00:18:10 +0900

    MVVM패턴 #15
    간단한 MVVM 패턴의 구현으로, Name 속성에 입력한 값을 UI에 표시

--- WpfApp241110/MainWindow.xaml.cs
+++ WpfApp241110/MainWindow.xaml.cs
@@ -1,49 +1,13 @@
-﻿using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Windows;
+﻿using System.Windows;
 
-namespace WpfApp241113
+namespace WpfApp241118
 {
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
-        private User _selectedUser;
-        public ObservableCollection<User> UserList { get; set; }
-
-        public User SelectedUser
-        {
-            get => _selectedUser;
-            set
-            {
-                _selectedUser = value;
-                OnPropertyChanged();
-            }
-        }
-
         public MainWindow ()
         {
             InitializeComponent();
-            UserList = new ObservableCollection<User>
-            {
-                new User { ID = 1, Name = "John Doe", Email = "john.doe@example.com", Phone = "123-456-7890" },
-                new User { ID = 2, Name = "Jane Smith", Email = "jane.smith@example.com", Phone = "098-765-4321" },
-                new User { ID = 3, Name = "Bob Johnson", Email = "bob.johnson@example.com", Phone = "555-123-4567" }
-            };
-            DataContext = this;
+            DataContext = new MainViewModel();
         }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged ([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
-
-    public class User
-    {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
     }
 }
