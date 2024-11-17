b5810b5142746e39d8a13adf5825ad715da3e4ca
dubukimch <101459905+dubukimch@users.noreply.github.com>
2024-11-18 00:20:30 +0900

    명령과 메서드 개념 #18
    사용자가 버튼을 클릭하면 메시지를 표시하는 명령

---
+++ WpfApp241110/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WpfApp241118
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand ShowNameCommand { get; }
+
+        public MainViewModel ()
+        {
+            ShowNameCommand = new RelayCommand(ShowName);
+        }
+
+        private void ShowName ()
+        {
+            MessageBox.Show($"Hello, {Name}!");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged ([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- WpfApp241110/MainWindow.xaml.cs
+++ WpfApp241110/MainWindow.xaml.cs
@@ -1,32 +1,13 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Windows;
+﻿using System.Windows;
 
-namespace WpfApp241117
+namespace WpfApp241118
 {
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
-        private string _name;
-        public string Name
-        {
-            get => _name;
-            set
-            {
-                _name = value;
-                OnPropertyChanged();
-            }
-        }
-
         public MainWindow ()
         {
             InitializeComponent();
-            DataContext = this;
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged ([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            DataContext = new MainViewModel();
         }
     }
 }
