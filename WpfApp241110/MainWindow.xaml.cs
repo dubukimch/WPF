3955741150e8358776771386d1dbb0029102a717
dubukimch <101459905+dubukimch@users.noreply.github.com>
2024-12-03 01:11:02 +0900

    버블 이벤트와 터널 이벤트 #40
    1. 버블 이벤트:
    이벤트가 자식에서 시작해 부모로 전파됩니다.
    실행 순서: Button → Grid → Window.
    2. 터널 이벤트:
    이벤트가 부모에서 시작해 자식으로 전파됩니다.
    실행 순서: Window → Grid → Button.

--- WpfApp241110/MainWindow.xaml.cs
+++ WpfApp241110/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 using WpfApp241110;
 
 namespace WpfApp241124
@@ -8,6 +9,24 @@ namespace WpfApp241124
         public MainWindow ()
         {
             InitializeComponent();
+        }
+
+        // Window에서 터널링 이벤트 발생
+        private void Window_PreviewMouseLeftButtonDown (object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Window PreviewMouseLeftButtonDown (Tunneling)");
+        }
+
+        // Grid로 이벤트 전파
+        private void Grid_PreviewMouseLeftButtonDown (object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Grid PreviewMouseLeftButtonDown (Tunneling)");
+        }
+
+        // Button으로 이벤트 전파
+        private void Button_PreviewMouseLeftButtonDown (object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Button PreviewMouseLeftButtonDown (Tunneling)");
         }
     }
 }
