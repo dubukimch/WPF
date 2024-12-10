f8115be031b9ec97f6cc94ce64968096df856041
dubukimch <101459905+dubukimch@users.noreply.github.com>
2024-12-11 02:16:43 +0900

    syncfusion의 mapchart 예시 #42
    syncfusion의 wpf로 mapchart 생성

---
+++ SynfusionWpfApp241129/SynfusionWpfApp241129/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using Syncfusion.Licensing;
+using System.Windows;
+
+public partial class App : Application
+{
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NDaF1cWWhIfEx1RHxQdld5ZFRHallYTnNWUj0eQnxTdEFiWH1WcnRXRmRVV01wVw==");
+        base.OnStartup(e);
+    }
+}
