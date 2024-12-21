8b1f0c6789a827d85a649e11334bdfae0853535d
dubukimch <101459905+dubukimch@users.noreply.github.com>
2024-12-21 13:44:54 +0900

    syncfusion의 bar chart #47
    syncfusion.chart를 이용해서 barchart 생성

---
+++ SynfusionWpfApp241129/SynfusionWpfApp241129/ChartViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SynfusionWpfApp241129
+{
+    public class ChartViewModel
+    {
+        public ObservableCollection<SalesInfo> SalesData { get; set; }
+
+        public ChartViewModel ()
+        {
+            // 데모 데이터 추가
+            SalesData = new ObservableCollection<SalesInfo>
+            {
+                new SalesInfo { Category = "Electronics", Sales = 15000 },
+                new SalesInfo { Category = "Groceries", Sales = 12000 },
+                new SalesInfo { Category = "Clothing", Sales = 8000 },
+                new SalesInfo { Category = "Furniture", Sales = 10000 }
+            };
+        }
+    }
+    public class SalesInfo
+    {
+        public string Category { get; set; }
+        public double Sales { get; set; }
+    }
+}
